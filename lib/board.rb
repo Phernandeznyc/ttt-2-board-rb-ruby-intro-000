385735382d5eb345e46dec3286b9f130cddb2bb7
P <phernandeznyc@gmail.com>
2020-12-25 22:48:45 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,10 +1,1 @@
-board = Array.new #=>[" "," "," "," "," "," "," "," "," "]
-row = ["1","2","3"]
-# Position 1: Left
-row[0]
-
-# Position 2: Middle
-row[1]
-
-# Position 3: Right
-row[2]
+board = Array.new #=>(9, " ")
